94823fac2062f583cee5423f09525ed0de9f35cd
Jayakumar <jai75984@gmail.com.com>
2022-01-21 02:11:28 +0530

    first Commit

---
+++ src/main/java/com/tatacliq/SpringBootCallingExternalApi3Application.java
@@ -0,0 +1,13 @@
+package com.tatacliq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCallingExternalApi3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCallingExternalApi3Application.class, args);
+	}
+
+}
